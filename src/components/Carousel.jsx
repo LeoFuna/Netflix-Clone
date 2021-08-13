0e9163d047a0ef79411ae6feb4cfadd43e27ea73
LeoFuna <leo.funa@gmail.com>
2021-08-13 15:08:43 -0300

    Requisiçao das categorias e passagem via prop para Carrossel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Carousel() {
+function Carousel( { genreId } ) {
   return (
-    <div>Sou o Carrossel</div>
+    <div>
+      { genreId }
+    </div>
   )
 }
+
+Carousel.propTypes = {
+  genreId: PropTypes.string.isRequired,
+};
 
 export default Carousel;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import HeroBanner from "../components/HeroBanner";
 import Carousel from "../components/Carousel";
+import { fetchAPI } from "../services";
 
 function Main() {
+  const [genres, setGenres] = useState([]);
+
+  useEffect(async () => {
+    const genresFromApi = await fetchAPI('/genre/movie/list?');
+    setGenres(genresFromApi.genres);
+  }, []);
+  
+  if (genres.length !== 0) {
+    return (
+      <div>
+        <Header />
+        <HeroBanner />
+        {genres.map((genre) => <Carousel key={ genre.id } genreId={ genre.id } /> )}
+      </div>
+    )
+  }
   return (
-    <div>
-      <Header />
-      <HeroBanner />
-      <Carousel />
-    </div>
+    <div>LOADING...</div>
   )
+
 }
 
 export default Main;
