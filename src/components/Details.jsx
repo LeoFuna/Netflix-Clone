bb5e18ad8d036fb882560649cf4fd0deb500c745
LeoFuna <leo.funa@gmail.com>
2021-09-10 14:19:43 -0300

    resoluçao de conflitos no componente Details que deixavam logs de erro

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from 'react';
-import PropTypes from 'prop-types';
 import NetflixContext from '../Context/NetflixContext';
 import { DetailsDiv } from '../styles/MainStyles';
 
@@ -12,7 +11,7 @@ function Details() {
   return (
     <DetailsDiv showDetails={ detailsVisibility }>
       <div>
-        <img src={`https://image.tmdb.org/t/p/original${itemToRender.backdrop_path}`} style={{ width: '40vw' }} />
+        {itemToRender.backdrop_path ? <img src={`https://image.tmdb.org/t/p/original${itemToRender.backdrop_path}`} style={{ width: '40vw' }} /> : ''}
         <h1>{'name' in itemToRender ? itemToRender.name : itemToRender.title}</h1>
         <p>{ itemToRender.overview }</p>
         <p>{ itemToRender.vote_average * 10 }% relevante</p>
@@ -26,14 +25,6 @@ function Details() {
       <button>Assistir Depois</button>
     </DetailsDiv>
   )
-}
-
-Details.propTypes = {
-  itemToDetail: PropTypes.shape({
-    id: PropTypes.number,
-    mediaType: PropTypes.string
-  }).isRequired,
-  showDetails: PropTypes.bool.isRequired
 }
 
 export default Details;
