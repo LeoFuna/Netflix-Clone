1b678056045165f9c564e932445ed7371cad7a70
LeoFuna <leo.funa@gmail.com>
2021-09-18 09:26:22 -0300

    Interaçao de add e remove filmes no assitir depois usando o provider adicionado no botao dos detalhes

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -7,7 +7,7 @@ import { DetailsDiv, ImageBackgroundOnDetails } from '../styles/MainStyles';
 
 function Details() {
   const [itemToRender, setItemToRender] = useState({});
-  const { itemToRenderOnDetail, detailsVisibility, handleShowDetails, handleLikeAndDislike, likedItems, dislikedItems } = useContext(NetflixContext);
+  const { itemToRenderOnDetail, detailsVisibility, handleShowDetails, handleLikeAndDislike, likedItems, dislikedItems, handleWatchAfterList } = useContext(NetflixContext);
   useEffect(() => {
     setItemToRender(itemToRenderOnDetail);
   }, [itemToRenderOnDetail]);
@@ -24,7 +24,7 @@ function Details() {
                       <h1>{ itemToRender.title }</h1>
                     </div>
                     <div>
-                      <button className="watch-after-details-button">
+                      <button onClick={ () => handleWatchAfterList(itemToRender) } className="watch-after-details-button">
                         <FontAwesomeIcon icon={ faPlay } />
                         <p>Assistir depois</p>
                       </button>
@@ -83,7 +83,7 @@ function Details() {
                   <h1>{ itemToRender.name }</h1>
                 </div>
                 <div>
-                  <button className="watch-after-details-button">
+                  <button onClick={ () => handleWatchAfterList(itemToRender) } className="watch-after-details-button">
                     <FontAwesomeIcon icon={ faPlay } />
                     <p>Assistir depois</p>
                   </button>
